664f58abe8b995bdb49ebcb51cb9e947a333adb8
Drey <84332780+Dreycod@users.noreply.github.com>
2023-11-30 19:39:38 +0100

    Test

--- WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/MainWindow.xaml.cs
@@ -23,113 +23,16 @@ namespace WeatherApp
     /// </summary>
     /// 
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-    public class Clouds
-    {
-        public int all { get; set; }
-    }
-
-    public class Coord
-    {
-        public double lon { get; set; }
-        public double lat { get; set; }
-    }
-
-    public class Main
-    {
-        public double temp { get; set; }
-        public double feels_like { get; set; }
-        public double temp_min { get; set; }
-        public double temp_max { get; set; }
-        public int pressure { get; set; }
-        public int humidity { get; set; }
-    }
-
-    public class Root
-    {
-        public Coord coord { get; set; }
-        public List<Weather> weather { get; set; }
-        public string @base { get; set; }
-        public Main main { get; set; }
-        public int visibility { get; set; }
-        public Wind wind { get; set; }
-        public Clouds clouds { get; set; }
-        public int dt { get; set; }
-        public Sys sys { get; set; }
-        public int timezone { get; set; }
-        public int id { get; set; }
-        public string name { get; set; }
-        public int cod { get; set; }
-    }
-
-    public class Sys
-    {
-        public int type { get; set; }
-        public int id { get; set; }
-        public string country { get; set; }
-        public int sunrise { get; set; }
-        public int sunset { get; set; }
-    }
-
-    public class Weather
-    {
-        public int id { get; set; }
-        public string main { get; set; }
-        public string description { get; set; }
-        public string icon { get; set; }
-    }
-
-    public class Wind
-    {
-        public double speed { get; set; }
-        public int deg { get; set; }
-    }
-
-
-
+ 
 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            WeatherAsync();
         }
 
-        public async void WeatherAsync()
-        {
-            Root root = await GetWeather();
-
-            TB_CurrentTemperature.Text = $"Temperature {root.main.temp.ToString()}°C";
-            TB_Humidite.Text = $"Precipitation: {root.main.humidity.ToString()}%";
-            TB_MaxTemperature.Text = $"Temperature Max: {root.main.temp_max.ToString()}°C";
-            TB_MiniTemperature.Text = $"Temperature Min: {root.main.temp_min.ToString()}°C";
-            TB_Meteo.Text = $"Meteo: {root.weather[0].description.ToString()}";
-            TB_Ressenti.Text = $"Ressenti: {root.main.feels_like.ToString()}°C";
-        }
-
-        public async Task<Root> GetWeather()
-        {
-            HttpClient client = new HttpClient();
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync("https://api.openweathermap.org/data/2.5/weather?q=annecy,fr&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric");
-                
-                if (response.IsSuccessStatusCode)
-                {
-                    string content = await response.Content.ReadAsStringAsync();
-                    Root root = JsonConvert.DeserializeObject<Root>(content);
-                    return root;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch
-            {
-                return null;
-            }
-        }
+       
 
     }
 }
